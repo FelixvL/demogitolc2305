47c602c192b6c0d07b259d24621d79f5d4ebf665
FelixvL <digitaleservice@gmail.com>
2023-05-23 12:57:29 +0200

    cors erbij

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 #flask run    :::   python -m flask run
 
 from flask import Flask
-
+from flask_cors import CORS, cross_origin
 app = Flask(__name__)
+cors = CORS(app)
 
 @app.route("/")
 def hello_world():
