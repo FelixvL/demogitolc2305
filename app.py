e240c34dfc1fc20bca715d61b1d0819d5ffa8889
FelixvL <digitaleservice@gmail.com>
2023-05-23 11:51:45 +0200

    bericht

---
+++ app.py
@@ -0,0 +1,21 @@
+#flask run    :::   python -m flask run
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+@app.route("/felix")
+def vanfelix():
+    getal = 4
+    resultaat = 3* getal
+    return str(resultaat)
+
+@app.route("/felix2/<voornaam>")
+def vanfelix2(voornaam):
+    getal = 4
+    resultaat = 3* getal
+    return "hoi "+voornaam
---
+++ felixbestand.py
@@ -0,0 +1,2 @@
+def telopdatgetal():
+    print("er wordt opgeteld")
